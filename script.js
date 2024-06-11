4734799364b0cdace2961001b793ccc0899d2e78
Richard Hocking <141461457+rhocking122@users.noreply.github.com>
2024-06-11 12:01:58 -0500

    update step 60

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const character = "#";
 const count = 8;
 const rows = [];
-function padRow(){
-    return "Hello!"
-}
-padRow();
-const call = padRow()
 
-console.log(call);
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount-rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount-rowNumber);
+}
+
 
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i+1));
+  rows.push(padRow(i+1,count))
 }
+
 let result = ""
-for (const row of rows){
+
+for (const row of rows) {
   result = result + "\n" + row;
 }
 
-console.log(result)
+console.log(result);
