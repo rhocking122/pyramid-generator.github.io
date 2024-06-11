0f02b31400e22c0e01366703846f3b5f888a0ce3
Richard Hocking <141461457+rhocking122@users.noreply.github.com>
2024-06-11 18:30:44 -0500

    completed the freecodecampl project

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-const character = "#";
-const count = 8;
+const character = "!";
+const count = 10;
 const rows = [];
+let inverted = false;
 
 function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount-rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount-rowNumber);
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// TODO: Use a different type of loop
-
-/*for (let i = 1; i <= count; i ++) {
-  rows.push(padRow(i,count))
-}*/
-
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
-
-/*for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
-}*/
-
-const numbers= [1,2,3];
-console.log(numbers)
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
 
 let result = ""
 
